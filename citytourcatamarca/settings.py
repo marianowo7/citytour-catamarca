5f097d15564a867148879df415c268f411a6090a
Mateo Bustamante <mateogbustamante@gmail.com>
2025-10-15 16:21:44 -0300

    primeros modelos

--- citytourcatamarca/settings.py
+++ citytourcatamarca/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'puntos_destacados',
+    'recorridos',
+    'unidades',
 ]
 
 MIDDLEWARE = [
@@ -102,19 +105,24 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'es-ar'
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
 USE_I18N = True
-
 USE_TZ = True
+
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- unidades/models.py
+++ unidades/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Unidad(models.Model):
+
+    patente = models.CharField(max_length=10, unique=True)
+    cantidad_asientos = models.PositiveIntegerField()
+
+    ESTADOS = [
+        ('ACT', 'Activa'),
+        ('MAN', 'En Mantenimiento'),
+        ('INA', 'Inactiva'),
+    ]
+    estado = models.CharField(max_length=3, choices=ESTADOS, default='ACT')
+
+    def __str__(self):
+        return f"{self.patente} ({self.get_estado_display()})"
